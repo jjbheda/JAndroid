91b1c6ac67ddf59a1f9e6c1dd0b9bbc8f3ec4cb2
jiangjingbo <jiangjingbo@qiyi.com>
2020-04-23 14:11:30 +0800

    盛水最多的容器，翻转链表

---
+++ datastructure/src/main/java/com/gavin/datastructure/leetcode/翻转链表.java
@@ -0,0 +1,47 @@
+package com.gavin.datastructure.leetcode;
+
+public class 翻转链表 {
+    static class ListNode {
+        int val;
+       ListNode next;
+       ListNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode one = new ListNode(2);
+        ListNode two = new ListNode(3);
+
+        head.next = one;
+        one.next = two;
+
+        ListNode pre = reverseList2(head);
+
+       while (pre != null) {
+           System.out.println(pre.val);
+           pre = pre.next;
+       }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+        return prev;
+    }
+
+    public static ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+
+}

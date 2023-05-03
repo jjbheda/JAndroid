0842112b6f34fed5daa090f64c87463b5271f47a
jiangjingbo <jiangjingbo@qiyi.com>
2023-05-03 16:19:28 +0800

    删除相同元素

---
+++ datastructure/src/main/java/com/gavin/datastructure/leetcode/easy/删除相同元素.java
@@ -0,0 +1,32 @@
+package com.gavin.datastructure.leetcode.easy;
+
+/**
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+ *
+ * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+ *
+ * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+ */
+class 删除相同元素 {
+
+    public static int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j ++) {
+            if(nums[i] !=  nums[j]) {
+                nums[i+1] =  nums[j];
+                i++;
+            }
+        }
+        for (int k = 0;k < i; k++) {
+            System.out.println(nums[k]);
+        }
+        return i;
+    }
+}
